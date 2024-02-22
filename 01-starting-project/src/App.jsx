b8c876bd367b5733711c3108d70683b34365e5c9
huntmori <huntmori04x22@gmail.com>
2024-02-23 01:53:49 +0900

    51. 이벤트 함수에 커스텀 인자 전달하기

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -8,8 +8,9 @@ import TabButton from './components/TabButton/TabButton.jsx';
 
 function App() {
 
-  let handleSelect = function() {
-    console.log('hello world');
+  let handleSelect = function(selectedButton) {
+    // selectedButton => 'components', 'jsx', 'props', 'state'
+    console.log(selectedButton);
   };
 
   return (
@@ -40,10 +41,22 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            <TabButton 
+              onSelect={() => handleSelect('components')}>
+              Components
+            </TabButton>
+            <TabButton 
+              onSelect={() => handleSelect('jsx')}>
+              JSX
+            </TabButton>
+            <TabButton 
+              onSelect={() => handleSelect('props')}>
+              Props
+            </TabButton>
+            <TabButton 
+              onSelect={() => handleSelect('state')}>
+              State
+            </TabButton>
           </menu>
           Dynamic Content
         </section>
