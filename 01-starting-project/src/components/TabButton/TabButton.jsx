4235d99b388ca4047b69c65f17d93842307216e0
huntmori <huntmori04x22@gmail.com>
2024-02-29 05:11:12 +0900

    66. 여러 JSX 슬롯 활용법

--- 01-starting-project/src/components/TabButton/TabButton.jsx
+++ 01-starting-project/src/components/TabButton/TabButton.jsx
@@ -1,10 +1,10 @@
 //export default function TabButton(props) {
-export default function TabButton({ children, onSelect, isSelected }) {
+export default function TabButton({ children, isSelected, ...props }) {
     console.log('TabButton COMPONENT EXECUTING');
 
     return (
         <li>
-            <button className={isSelected ? 'active' : ''} onClick={onSelect}>
+            <button className={isSelected ? 'active' : ''} { ...props }>
                 {children}
             </button>
         </li>
