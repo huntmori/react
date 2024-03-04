968673f91d479552bccaef3eadf69b90e53da071
huntmori <huntmori04x22@gmail.com>
2024-03-04 18:53:05 +0900

    74. 개념 복습: State(상태) 활용법

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,14 +1,49 @@
+import { useState } from "react";
 
 export default function Player({name, symbol}) {
+
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEditClick() {
+        setIsEditing(true);
+    }
+
+    let playerName = <span className="player-name">{name}</span>;
+
+    if (isEditing === true) {
+        playerName = <input type="text" required />;
+    }
+
     return (
         <>
             <li>
                 <span className="player">
-                <span className="player-name">{name}</span>
-                <span className="player-symbol">{symbol}</span>
+                    {playerName}
+                    <span className="player-symbol">{symbol}</span>
                 </span>
-                <button className="edit">Edit</button>
+                <button className="edit" onClick={handleEditClick}>Edit</button>
             </li>
         </>
     );
+
+    /*
+    return (
+        <>
+            <li>
+                <span className="player">
+                { !isEditing ? <span className="player-name">{name}</span> : '' }
+                { isEditing ? 
+                    <span className="player-name">
+                        <input type="text" value={name}/>
+                    </span> 
+                : ''}
+                <span className="player-symbol">{symbol}</span>
+                </span>
+                <button className="edit" onClick={()=>{setIsEditing(!isEditing)}}>
+                    { isEditing ? 'Save' : 'Edit' }
+                </button>
+            </li>
+        </>
+    );
+    */
 }
