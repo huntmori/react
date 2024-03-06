9b24b81e71fcaad5a70f5c1814d568a2d3e0ea6c
huntmori <huntmori04x22@gmail.com>
2024-03-06 15:34:31 +0900

    81. State(상태) 끌어올리기 [핵심 개념]

--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -6,16 +6,18 @@ const initialGameBoard = [
     [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
 
     const [ gameBoard, setGameBoard] = useState(initialGameBoard);
 
     const handleSelectSquare = function (rowIndex, colIndex) {
         setGameBoard((prevGameBoard) => {
             const updatedBoard = [...prevGameBoard].map(innerArray => [...innerArray]);
-            updatedBoard[rowIndex][colIndex] = 'X';
+            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
             return updatedBoard;
         })
+
+        onSelectSquare();
     } 
 
     return <>
