99b195f4b1f12ad13df54dca35cb6a86dced07de
huntmori <huntmori04x22@gmail.com>
2024-03-27 20:17:20 +0900

    121. 테일윈드 CSS로 데모 앱 옮기기

--- 07-styling/01-starting-project/src/components/AuthInputs.jsx
+++ 07-styling/01-starting-project/src/components/AuthInputs.jsx
@@ -1,37 +1,7 @@
 import { useState } from 'react';
-import { styled } from 'styled-components';
 
 import Button from "./Label.jsx";
 import Input from "./Input.jsx";
-
-const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`
-
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${({$invalid})=> ($invalid ? '#f87171' : '#6b7280') };
-`
-//
-// const Input = styled.input`
-//   width: 100%;
-//   padding: 0.75rem 1rem;
-//   line-height: 1.5;
-//   border: 1px solid transparent;
-//   border-radius: 0.25rem;
-//   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-//   color: ${({ $invalid }) => ($invalid ? '#ef4444' : '#374151') };
-//   border-color: ${ ({ $invalid }) => ( $invalid ? '#f73f3f' : 'none' ) };
-//   background-color: ${ ({ $invalid }) => ( $invalid ? '#fed2d2' : '#d1d5db' )};
-// `
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -54,8 +24,8 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div id="auth-inputs" className="w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800">
+      <div className="flex flex-col gap-2 mb-6">
         <Input
           label="Email"
           invalid={emailNotValid}
@@ -70,9 +40,9 @@ export default function AuthInputs() {
             handleInputChange('password', event.target.value)
           }}
         />
-      </ControlContainer>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
         <Button onClick={handleLogin}>Sign In</Button>
