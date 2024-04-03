3ad046010471db97cbb94722723b1ae9a83e11f2
huntmori <huntmori04x22@gmail.com>
2024-04-03 18:20:50 +0900

    130. 복습: State(상태)를 사용한 사용자 입력 관리 (양방향 바인딩)

--- 08 Refs Portals/01-starting-project/src/components/Player.jsx
+++ 08 Refs Portals/01-starting-project/src/components/Player.jsx
@@ -1,11 +1,26 @@
+import {useState} from "react";
+
 export default function Player() {
-  return (
+
+    const [enteredPlayerName, setEnteredPlayerName] = useState('');
+    const [ submitted, setSubmitted ] = useState(false);
+
+    const handleChange = function(event) {
+        setSubmitted(false);
+        setEnteredPlayerName(event.target.value);
+    }
+
+    const handleClick = function() {
+        setSubmitted(true);
+    }
+
+    return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome { submitted ? enteredPlayerName : 'unknown entity' }</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={enteredPlayerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
-  );
+    );
 }
